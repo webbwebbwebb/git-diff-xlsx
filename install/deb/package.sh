c599efad554c432a2e2749dc4dda117803fd5db9
mark.webb <mark@webbwebbwebb.co.uk>
2020-06-16 13:31:13 +0100

    Debian package script now extracts version number from payload

--- install/deb/package.sh
+++ install/deb/package.sh
@@ -3,11 +3,22 @@ package_dir="$script_dir"/git-diff-xlsx
 source_dir="$script_dir"/../../src/git-diff-xlsx/bin/Release/netcoreapp3.1/publish
 output_dir="$package_dir"/usr/lib/git-diff-xlsx
 
-sudo chmod -R 0777 "$package_dir"
+# determine version number
+version_number="$(grep -Po -m1 '(?<=git-diff-xlsx\/)(\d+\.)?(\d+\.)?(\d+)' "$source_dir/git-diff-xlsx.deps.json")"
+if [ -z "$version_number" ]; then
+    echo "Cannot determine version number"
+    exit 1
+fi
+echo "Version number is $version_number"
 
-# Purge output directory
-rm -rf "$output_dir"
+# Purge package directory
+rm -rf "$package_dir"
+mkdir -p "$package_dir"
+mkdir -p "$package_dir/DEBIAN/"
 mkdir -p "$output_dir"
+
+# Create control file from template
+sed "s/##<VERSION_NUMBER>##/$version_number/g" "$script_dir/control.template" > "$package_dir/DEBIAN/control"
 
 # Copy contents of publish directory
 rsync -avr --exclude="*.exe" "$source_dir"/ "$output_dir"
