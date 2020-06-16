257a893b2ba0abb9934aa77184444eeabcb3d907
mark.webb <mark@webbwebbwebb.co.uk>
2020-06-16 13:28:54 +0100

    Chocolatey package script now extracts version number from payload

--- install/chocolatey/package.ps1
+++ install/chocolatey/package.ps1
@@ -3,6 +3,16 @@ $package_dir = "$script_dir\git-diff-xlsx"
 $source_dir = "$script_dir\..\..\src\git-diff-xlsx\bin\Release\netcoreapp3.1\publish"
 $output_dir = "$package_dir\tools"
 
+# determine version number
+$package_information = Get-Content "$source_dir\git-diff-xlsx.deps.json" | Out-String
+
+if($package_information -match '(?<=git-diff-xlsx\/)(\d+\.)?(\d+\.)?(\d+)') {
+    $version_number = $Matches[0]
+    Write-Output "Version number is $version_number"
+} else {
+    throw 'Cannot determine version number'
+}
+
 # Purge output directory
 remove-item "$output_dir" -recurse -force
 
@@ -15,4 +25,4 @@ Copy-Item "$package_dir\VERIFICATION.txt" "$output_dir\VERIFICATION.txt"
 Remove-Item "$script_dir\*.nupkg"
 
 # Create package
-choco pack git-diff-xlsx/git-diff-xlsx.nuspec
+choco pack --version $version_number git-diff-xlsx/git-diff-xlsx.nuspec
